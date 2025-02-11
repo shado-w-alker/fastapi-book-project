e28b68d6af1d3b8e5a6cf5ea5aacb9a00b82f921
shado-w-alker <mecdiebere@gmail.com>
2025-02-11 06:08:06 +0100

    Testing CI

--- tests/test_books.py
+++ tests/test_books.py
@@ -3,7 +3,7 @@ from tests import client
 
 def test_get_all_books():
     response = client.get("/books/")
-    assert response.status_code == 200
+    assert response.status_code == 404
     assert len(response.json()) == 3
 
 
